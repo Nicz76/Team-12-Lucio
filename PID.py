c7df81fe5b49d69fc2dc6b4de0ada54fe8bfdba8
Nicz76 <130241400+Nicz76@users.noreply.github.com>
2024-05-10 15:18:50 -0700

    Update PID.py

--- PID.py
+++ PID.py
@@ -1,9 +1,14 @@
 import time
+import numpy as np
+import scipy.integrate import odeint
 
 previous_error = 0
 integral = 0
 i = 0
 setpoint = 20
+Kp = 1
+Ki = 1
+Kd = 1
 dt = 0.1
 for (i < 10):
   error = (setpoint - previous error)/ dt
